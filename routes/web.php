2363788eb7cbb840c669db970c4489e440722b51
altantic <15200582@life.hlkbu.edu.hk>
2020-03-11 03:56:59 +0800

    Apply Eloquent on Route [Read] - [find, findOrFail, firstOrFail]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -75,3 +75,45 @@ Route::get('rawSQL/delete', function(){
 });
 
 
+/*
+|--------------------------------------------------------------------------
+| ELOQUENT
+|--------------------------------------------------------------------------
+*/
+Route::get('/read', function(){
+   $posts = Post::all();
+
+   foreach($posts as $post) {
+      return $post->title;
+   }
+});
+
+Route::get('/find', function(){
+   $post = Post::find(2);               // where id = 2
+   return $post->title;
+
+   /*
+   $posts = Post::all();                // Display psot title, where id = 1
+   foreach($posts as $post) {
+     return $post->title;
+   }
+   */
+});
+
+Route::get('/findwhere', function(){
+   $posts = Post::where('id', 2)
+                ->orderBy('id', 'desc')
+                ->take(1)
+                ->get();
+   return  $posts;
+});
+
+Route::get('/findmore', function(){
+   // Find Post with id = 2, or return 404 page by ModelNotFoundException
+   $post2 = Post::findOrFail(2);
+
+   // Find Post with id = 1, or return 404 page by ModelNotFoundException
+   $post1 = Post::where('id', '<', 50)->firstOrFail();
+
+   return "First title: ". $post1->title."<br>Second Title: ".$post2->title;
+});
