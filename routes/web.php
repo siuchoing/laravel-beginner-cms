8d265a4e92a5f959e12e3c46d4e0b2e28a05457e
altantic <15200582@life.hlkbu.edu.hk>
2020-03-20 08:59:41 +0800

    Remark for "web" middleware on route

--- routes/web.php
+++ routes/web.php
@@ -359,3 +359,24 @@ Route::group(['middleware'=>'web'], function(){
   });
 });
 
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+//Route::group(['middleware' => ['web']], function () {
+//
+//
+//
+//
+//
+//});
