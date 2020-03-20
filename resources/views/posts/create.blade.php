e7162324650a31f76819c89ce278b2169ae09f35
altantic <15200582@life.hlkbu.edu.hk>
2020-03-21 01:08:53 +0800

    Create Error message for Form Input

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -12,11 +12,26 @@
                 <label for="title">Title</label>
                 <input type="text" class="form-control" name="title" placeholder="Enter title">
             </div>
-            <div></div>
+            @if($errors->has('title'))
+                <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                    {{$errors->first('title')}}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
             <div class="form-group">
                 <label for="content">Content</label>
                 <input type="text" class="form-control" name="content" placeholder="Enter content">
             </div>
+            @if($errors->has('content'))
+                <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                    {{$errors->first('content')}}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
             <button type="submit" class="btn btn-primary" style="float: right">Submit</button>
         </form>
     </div>
