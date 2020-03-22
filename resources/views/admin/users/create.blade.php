f33b244217d7a9d4579818b0006aaef3785fb891
altantic <15200582@life.hlkbu.edu.hk>
2020-03-23 00:58:36 +0800

    Implement create page for admin user

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,4 +1,56 @@
 @extends('layouts.admin')
 @section('content')
     <h1>Create User</h1>
+
+    <form method="POST" action="/admin/users" accept-charset="UTF-8">
+
+        {{csrf_field()}}
+
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input class="form-control" placeholder="Enter name" name="name" type="text" id="name">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input class="form-control" placeholder="Enter email" name="email" type="text" id="email">
+        </div>
+
+        <div class="form-group">
+            <label for="role_id">Role:</label>
+            <select class="form-control" id="role_id" name="role_id">
+                <option value="" selected="selected">Choose Options</option>
+                @foreach($roles as $id => $name)
+                    <option value="$id">{{ $name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="status">Status:</label>
+            <select class="form-control" id="status" name="status">
+                <option value="1">Active</option>
+                <option value="0" selected="selected">No Active</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="country_id">Countries:</label>
+            <select class="form-control" id="country_id" name="country_id">
+                <option value="" selected="selected">Choose Options</option>
+                @foreach($countries as $id => $name)
+                    <option value="$id">{{ $name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input class="form-control" placeholder="Enter password" name="password" type="password" value="" id="password">
+        </div>
+
+        <div class="form-group">
+            <input class="btn btn-primary" type="submit" value="Create User">
+        </div>
+    </form>
 @stop
