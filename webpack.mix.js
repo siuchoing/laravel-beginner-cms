da0dc93ebddf0c698fa9c7a49903a58774764f75
altantic <15200582@life.hlkbu.edu.hk>
2020-03-22 14:56:52 +0800

    1. Apply styles and scripts method in webpack file 2. npm run dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,24 @@ const mix = require('laravel-mix');
  |
  */
 
+// Compile Sass to CSS [from Resources to Public folder]
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+// Styles Method: Concatenate some plain CSS stylesheets into a single file
+mix.styles([
+    'resources/assets/css/libs/blog-post.css',
+    'resources/assets/css/libs/bootstrap.css',
+    'resources/assets/css/libs/font-awesome.css',
+    'resources/assets/css/libs/metisMenu.css',
+    'resources/assets/css/libs/sb-admin-2.css'
+], 'public/css/libs.css');
+
+// Scripts Method: combine and minify any JavaScript files
+mix.scripts([
+    'resources/assets/js/libs/jquery.js',
+    'resources/assets/js/libs/bootstrap.js',
+    'resources/assets/js/libs/sb-admin-2.js',
+    'resources/assets/js/libs/metisMenu.js',
+    'resources/assets/js/libs/scripts.js'
+], 'public/js/libs.js');
