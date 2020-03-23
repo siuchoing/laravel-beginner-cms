a58190364cfc3dbe94988557fec97fcc545a9237
altantic <15200582@life.hlkbu.edu.hk>
2020-03-24 00:27:16 +0800

    Apply usersRequest for admin user for input validation

--- app/Http/Requests/UsersRequest.php
+++ app/Http/Requests/UsersRequest.php
@@ -13,7 +13,7 @@ class UsersRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,12 @@ class UsersRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|email|max:255|unique:users',
+            'role_id' => 'required',
+            'country_id' => 'required',
+            'is_active' => 'required',
+            'password' => 'required'
         ];
     }
 }
