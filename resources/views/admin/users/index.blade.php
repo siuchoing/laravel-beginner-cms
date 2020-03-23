3f9b8bf9205052210fb9dc03f815974915241b99
altantic <15200582@life.hlkbu.edu.hk>
2020-03-23 21:35:02 +0800

    Fixed error due to null role_id

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -21,7 +21,7 @@
                     <td>{{$user->id}}</td>
                     <td>{{$user->name}}</td>
                     <td>{{$user->email}}</td>
-                    <td>{{$user->role->name}}</td>
+                    <td>{{!empty($user->role->name) ? $user->role->name : '--' }}</td>
                     <td>{{$user->is_active == 1 ? 'Active' : 'No Active'}}</td>
                     <td>{{$user->created_at ? $user->created_at->diffForHumans() : '--'}}</td>
                     <td>{{$user->updated_at ? $user->updated_at->diffForHumans() : '--' }}</td>
